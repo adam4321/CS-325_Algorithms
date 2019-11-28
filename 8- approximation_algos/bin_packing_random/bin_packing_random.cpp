529d95300bdcf615e1175f82027a09580f084502
Wright <wrighada@oregonstate.edu>
2019-11-27 23:25:32 -0800

    add random bin packing program

--- 8- approximation_algos/bin_packing_random/bin_packing_random.cpp
+++ 8- approximation_algos/bin_packing_random/bin_packing_random.cpp
@@ -1,7 +1,7 @@
 /******************************************************************************
 **  Author:       Adam Wright
 **  Dage:         11/26/2019
-**  Description:  Program that reads from a text file and then tests the 
+**  Description:  Program that creates random bins and then tests the 
 **                runtime of three different algorithms for solving the bin
 **                packing problem.
 ******************************************************************************/
@@ -134,93 +134,81 @@ int best_fit(vector<int> &weight, int n, int c)
 } 
 
 
-/*********************************************************************
-** Description:   Main function reads from the input file bin.txt
-*********************************************************************/
 int main()
 {
-    // Open the input file
-    ifstream in_file;
-    in_file.open("bin.txt");
+    // Seed the random function
+    srand(time(NULL));
+
+    // Declare the necessary containers
+    vector<int> item_weight_vector;
+    int num_items = 3000;
+    int capacity = 10;
+    int test_count = 1;
+    double time;
+    int iter = 1;
 
     cout << endl;
     cout << "*******************************************" << endl;
     cout << "*  Three greedy aproximations for          " << endl;
     cout << "*  solving the bin packing problem         " << endl;
-    cout << "**************************************" << endl << endl;
+    cout << "**************************************";
+    cout << endl << endl;
 
-    if (in_file.is_open())
+    // Run the sorting for 3,000 - 30,000
+    while (num_items <= 30000)
     {
-        int num_tests;
-        int capacity;
-        int num_items;
-        int item_weight;
-        int test_count = 1;
-        vector<int> item_weight_vector;
-
-        while (in_file >> num_tests)
+        // Fill the deque with random numbers 0 - 10,000
+        int i = 0;
+        while (i < num_items)
         {
-            for (int i = 0; i < num_tests; i++)
-            {
-                in_file >> capacity;
-                in_file >> num_items;
-
-                for (int i = 0; i < num_items; i++)
-                {
-                    in_file >> item_weight;
-                    item_weight_vector.push_back(item_weight);
-                }
-
-                // Start time first fit
-                auto start = high_resolution_clock::now();
-                int ff = first_fit(item_weight_vector, num_items, capacity);
-
-                // Stop time and calculate duration first fit
-                auto stop = high_resolution_clock::now();
-                auto run_time = duration_cast<microseconds>(stop - start);
-                // Convert microseconds to seconds
-                int ff_time = run_time.count() * 0.000001;
-
-                // Start time first fit descending
-                start = high_resolution_clock::now();
-                int ffd = first_fit_descending(item_weight_vector, num_items, capacity);
-
-                // Stop time and calculate duration first fit descenting
-                stop = high_resolution_clock::now();
-                run_time = duration_cast<microseconds>(stop - start);
-                int ffd_time = run_time.count() * 0.000001;
-
-                // Start time best fit
-                start = high_resolution_clock::now();
-                int bf = best_fit(item_weight_vector, num_items, capacity);
-
-                // Stop time and calculate duration best fit
-                stop = high_resolution_clock::now();
-                run_time = duration_cast<microseconds>(stop - start);
-                int bf_time = run_time.count() * 0.000001;
-                
-
-                cout << "Test Case: " << test_count << " ";
-                cout << "First Fit: " << ff << ", " <<  ff_time << "sec. ";
-                cout << "First Fit Decreasing: " << ffd << ", " <<  ffd_time << "sec. ";
-                cout << "Best Fit: " << bf << ", " <<  bf_time << "sec.";
-                cout << endl;
-
-                // Clear the vector and increment the test count
-                item_weight_vector.clear();
-                test_count++;
-            }
+            int random = rand() % 10000;
+            item_weight_vector.push_back(random);
+            i++;
         }
-    }
-    else
-    {
-        cout << "Error, the input file isn't open." << endl;
-    }
 
-    // Close the input file
-    in_file.close();
+
+        // Start time first fit
+        auto start = high_resolution_clock::now();
+        int ff = first_fit(item_weight_vector, num_items, capacity);
+
+        // Stop time and calculate duration first fit
+        auto stop = high_resolution_clock::now();
+        auto run_time = duration_cast<microseconds>(stop - start);
+        // Convert microseconds to seconds
+        int ff_time = run_time.count() * 0.000001;
+
+        // Start time first fit descending
+        start = high_resolution_clock::now();
+        int ffd = first_fit_descending(item_weight_vector, num_items, capacity);
+
+        // Stop time and calculate duration first fit descenting
+        stop = high_resolution_clock::now();
+        run_time = duration_cast<microseconds>(stop - start);
+        int ffd_time = run_time.count() * 0.000001;
+
+        // Start time best fit
+        start = high_resolution_clock::now();
+        int bf = best_fit(item_weight_vector, num_items, capacity);
+
+        // Stop time and calculate duration best fit
+        stop = high_resolution_clock::now();
+        run_time = duration_cast<microseconds>(stop - start);
+        int bf_time = run_time.count() * 0.000001;
+        
+
+        cout << "Test Case: " << test_count << " ";
+        cout << "First Fit: " << ff << ", " <<  ff_time << "sec. ";
+        cout << "First Fit Decreasing: " << ffd << ", " <<  ffd_time << "sec. ";
+        cout << "Best Fit: " << bf << ", " <<  bf_time << "sec.";
+        cout << endl;
+
+        // Clear the vector and increment the test count
+        item_weight_vector.clear();
+    }
 
     cout << endl;
+    cout << "*****************************";
+    cout << endl << endl;
 
     return 0;
 }
